fe5e2d9f7a1eba911e413704f2672a3e96ea5d80
teresiahkingori <trizahkingori@gmail.com>
2023-08-09 16:47:29 +0300

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(10):
     for j in range(i+1, 10):
         if i != 8 or j != 9:
             print("{}{}, ".format(i, j), end="")
-else:
-    print("{}{}".format(i, j))
+    else:
+        print("{}{}".format(i, j))
