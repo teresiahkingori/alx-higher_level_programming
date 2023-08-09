10d82cfbc53c0742051fb2145c0b499d1af7e343
teresiahkingori <trizahkingori@gmail.com>
2023-08-09 16:32:03 +0300

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+    num = 32
+else:
+   num = 0
+    print("{:c}".format(ord(str[i]) - num), end='')
+    print()
