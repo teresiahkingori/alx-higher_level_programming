ad4362a26bfe5fb9c9d0ddf06b51fa02e96940f4
teresiahkingori <trizahkingori@gmail.com>
2023-08-10 19:30:33 +0300

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+def main():
+    # Assign values to variables a and b
+    a = 1
+    b = 2
+    # Perform the addition and print the result
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+0x02. Python - import & modules
+0x02. Python - import & modules
+
