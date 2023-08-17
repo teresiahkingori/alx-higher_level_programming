faa54a3fabc6438ee743dbb2bf19fbd39b032cdf
teresiahkingori <trizahkingori@gmail.com>
2023-08-17 12:58:13 +0300

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+prints a dictionary by ordered keys
+"""
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+	value = a_dictionary[key]
+        print(f"{key}:", value)
