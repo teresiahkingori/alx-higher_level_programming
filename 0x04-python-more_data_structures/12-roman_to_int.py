3d8ea29ef6d7c7d7179c8bb39e2d3b36b8d63087
teresiahkingori <trizahkingori@gmail.com>
2023-08-17 14:52:26 +0300

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
+"""
+converts a Roman numeral to an integer
+"""
+
+
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return None
-    n = 0
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for i in roman_string:
-        n += roman.get(i, 0)
-    subs = {'CM': -200, 'CD': -200, 'XC': -20, 'XL': -20, 'IX': -2, 'IV': -2}
-    chars = ""
-    for i in range(0, len(roman_string) - 1):
-        chars = roman_string[i] + roman_string[i + 1]
-    if chars in subs:
-        n += subs[chars]
-    return n
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    for roman in reversed(roman_string):
+        numeral = digits[roman]
+        total += numeral if total < numeral * 5 else -numeral
+    return total
